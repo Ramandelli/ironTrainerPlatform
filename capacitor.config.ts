6cc0f121f35e3326c801e045ab1a9c75dbbd2602
Ramandelli <adramandelli@gmail.com>
2025-08-05 17:47:22 -0300

    Organiza estrutura iOS para Codemagic

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.5e6f770c7ed14412afc876151da22317',
-  appName: 'Iron Tracker',
+  appId: 'br.projeto.irontrainer',
+  appName: 'Iron Trainer',
   webDir: 'dist',
-  server: {
-    url: 'https://5e6f770c-7ed1-4412-afc8-76151da22317.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  
   plugins: {
     Preferences: {
       configure: {
